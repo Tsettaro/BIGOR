293db5f03b99d9243f5cfcc87516c377315ae38b
Artur <43388317+Tsettaro@users.noreply.github.com>
2023-01-02 15:49:26 +0300

    Edit functions

--- PTNH.py
+++ PTNH.py
@@ -7,6 +7,11 @@ from pathlib import Path
 
 pages = []
 
+def find(cl, soup):
+    links = soup.find_all('a', class_ = cl)
+    for i in links:
+        pages.append('http://bigor.bmstu.ru'+i.get('href'))
+
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
@@ -36,6 +41,13 @@ def inp():
 
     if dr[-1] != '/': dr+='/'
     return dr
+
+def setup(url):
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    find('eLModul', soup)
+    find('eExtern', soup)
+    find('eLTestMod', soup)
 
 def download(psr, link):
     r = requests.get(link) 
@@ -72,46 +84,38 @@ def sort(tag, source):
 def cls():
     os.system('cls' if platform.system()=='Windows' else 'clear')
 
-def find(cl):
-    links = soup.find_all('a', class_ = cl)
-    for i in links:
-        pages.append('http://bigor.bmstu.ru'+i.get('href'))
-
 if (platform.system() != "Windows" and platform.system() != "Linux"):
     print(f"I'm sorry, but I can't work in {platform.system()}. Please, contact to my developer for further information!")
     quit()
 
 dr = ''
+
 while (dr == ''):
     dr = inp()
+
 os.chdir(dr)
+
 if (os.path.exists("BIGOR_stable") == False):
     os.mkdir("BIGOR_stable")
     os.chdir(dr+"BIGOR_stable")
     os.mkdir('App')
+
 source = dr+"BIGOR_stable/App/my_site/bigor.bmstu.ru/"
+
 os.chdir(dr)
-
-
-url = 'http://bigor.bmstu.ru/?cnt/?doc=OP2/OP_T.cou'
-html_text = requests.get(url).text
-soup = BeautifulSoup(html_text, 'lxml')
 
 def fix_links(page):
     page = page.replace('/?cnt/?doc=OP2/', source+'cnt__doc_OP2_').replace('doc=OP2/','doc_OP2_')
     page = page.replace('/?cou=OP2/OP_T','__cou_OP2_OP_T')
     page = page.replace('.cou','.cou.html\"').replace('.mod_','.mod__').replace('___','__')
     page = page.replace('gif.gif', 'gif').replace('/?frm/','/').replace('.png', '')
-    page = page.replace('/?asr/',source+'asr_').replace('/?img/', '/').replace('/?','_').replace('img_','').replace('n_','n=')
-    page = page.replace('./1557202953', source+'cnt__doc_OP2_OP_T.cou.html\"')
+    page = page.replace('/?asr/',source+'asr_').replace('/?img/', '/').replace('/?','_').replace('img_','')
+    page = page.replace('./1557202953', source+'cnt__doc_OP2_OP_T.cou.html\"').replace('__n_','__n=').replace('.mod_n=','.mod__n=')
     return page
 
 print("Loading main page...")
-
-find('eLModul')
-find('eExtern')
-find('eLTestMod')
-
+setup('http://bigor.bmstu.ru/?cnt/?doc=OP2/OP_T.cou')
+setup('http://bigor.bmstu.ru/?cnt/?doc=OP2/OP_P.cou')
 print("Links to pages added.")
 print("Download pages...")
 
@@ -122,15 +126,14 @@ for i in pages:
         if images.endswith(".png"):
             os.remove(os.path.join(source, images))
 
-
-
 print("Pages successfully downloaded!")
+
 files = [f for f in os.listdir(source) if f.endswith('.gif.gif')]
+
 for i in range(len(files)):
     files[i] = files[i].replace('gif.gif', 'gif')
     if (os.path.isfile(source+files[i]) == 0):
         os.rename(source+files[i]+'.gif', source+files[i])
-
 
 for i in sort('html', source):
     HTMLFile = open(source+i, "r", encoding="utf-8")
